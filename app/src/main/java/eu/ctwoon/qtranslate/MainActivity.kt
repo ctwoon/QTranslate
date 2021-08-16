7f3df2fa57aa78113ddb6a494b4e1fb6538e1f10
Nick C <pokemonnekitgod@gmail.com>
2021-08-16 14:46:30 +0300

    initial style & skeleton

--- app/src/main/java/eu/ctwoon/qtranslate/MainActivity.kt
+++ app/src/main/java/eu/ctwoon/qtranslate/MainActivity.kt
@@ -2,10 +2,29 @@ package eu.ctwoon.qtranslate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<EditText>(R.id.ed).addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+            }
+        })
     }
 }
